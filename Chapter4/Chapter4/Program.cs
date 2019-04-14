02f406fa1038e96a77f79bbad8c38709be2409f3
James Duncan <duncanje501@gmail.com>
2019-04-14 17:56:52 -0600

    add p6

--- Chapter4/Chapter4/Program.cs
+++ Chapter4/Chapter4/Program.cs
@@ -10,10 +10,7 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-            //Write a program that reads from the console two integer numbers (int)
-            //and prints how many numbers between them exist, such that the
-            //remainder of their division by 5 is 0.Example: in the range(14, 25)
-            //there are 3 such numbers: 15, 20 and 25.
+            //Write a program that reads two numbers from the console and prints the greater of them. Solve the problem without using conditional statements.
         }
     }
 }
