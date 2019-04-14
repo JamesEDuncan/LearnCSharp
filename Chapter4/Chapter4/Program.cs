15cfe29c3217c074642d715a4dc53f09d33fcf94
James Duncan <duncanje501@gmail.com>
2019-04-14 17:27:27 -0600

    add p1

---
+++ Chapter4/Chapter4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that reads from the console three numbers of type int and prints their sum.
+        }
+    }
+}
