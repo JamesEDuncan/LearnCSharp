4384776cbd9c7e0469b25eab73ab7a2c565ac5bb
James Duncan <duncanje501@gmail.com>
2019-04-14 18:16:15 -0600

    sln p1

--- Chapter4/Chapter4/Program.cs
+++ Chapter4/Chapter4/Program.cs
@@ -11,6 +11,11 @@ namespace Chapter4
         static void Main(string[] args)
         {
             //Write a program that reads from the console three numbers of type int and prints their sum.
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int sum = a + b + c;
+            Console.WriteLine(sum);
         }
     }
 }
