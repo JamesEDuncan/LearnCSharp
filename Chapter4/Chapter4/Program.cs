e8c430ee63558bfaa91bffca5d75bb84a792336c
James Duncan <duncanje501@gmail.com>
2019-04-14 18:20:47 -0600

    sln p2

--- Chapter4/Chapter4/Program.cs
+++ Chapter4/Chapter4/Program.cs
@@ -11,6 +11,10 @@ namespace Chapter4
         static void Main(string[] args)
         {
             //Write a program that reads from the console the radius "r" of a circle and prints its perimeter and area.
+            double r = double.Parse(Console.ReadLine());
+            double parameter = 2 * Math.PI * r;
+            double area = Math.PI * r * r;
+            Console.WriteLine("The parameter is " + parameter + " and the area is " + area + ".");
         }
     }
 }
