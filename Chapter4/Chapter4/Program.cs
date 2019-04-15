5caa3880c3b0e5745c03a0a5b3581fc5ed395f85
James Duncan <duncanje501@gmail.com>
2019-04-14 18:13:42 -0600

    correct p3

--- Chapter4/Chapter4/Program.cs
+++ Chapter4/Chapter4/Program.cs
@@ -10,7 +10,7 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-            //@A given company has name, address, phone number, fax number, web site and manager.The manager has name, surname and phone number. Write a program that reads information about the 
+            //A given company has name, address, phone number, fax number, web site and manager.The manager has name, surname and phone number. Write a program that reads information about the 
             //company and its manager and then prints it on the console.Write a program that reads from the console the radius "r" of a circle and prints its perimeter and area.
         }
     }
