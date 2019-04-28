8409d82c8214a2f9c2a84acaa17fdf402e1b5317
James Duncan <duncanje501@gmail.com>
2019-04-28 14:06:48 -0600

    sln p6

--- Chapter4/Chapter4/Program.cs
+++ Chapter4/Chapter4/Program.cs
@@ -10,7 +10,14 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-            //Write a program that reads two numbers from the console and prints the greater of them. Solve the problem without using conditional statements.
+            // 6. Write a program that reads two numbers from the console and prints the greater of them. Solve the problem without using conditional statements.
+
+            Console.Write("Please type a number: ");
+            double firstNumber = double.Parse(Console.ReadLine());
+            Console.Write("Please type a different number: ");
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("The larger number is " + Math.Max(firstNumber, secondNumber) + ".");
         }
     }
 }
