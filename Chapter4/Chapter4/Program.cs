6ad075585ae78cba591798d863f523d2b3dcf615
James Duncan <duncanje501@gmail.com>
2019-04-27 19:24:26 -0600

    sln p3

--- Chapter4/Chapter4/Program.cs
+++ Chapter4/Chapter4/Program.cs
@@ -10,8 +10,33 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-            //A given company has name, address, phone number, fax number, web site and manager.The manager has name, surname and phone number. Write a program that reads information about the 
-            //company and its manager and then prints it on the console.Write a program that reads from the console the radius "r" of a circle and prints its perimeter and area.
+            //A given company has name, address, phone number, fax number, web site and manager. The manager has name, surname and phone number. Write a program that reads information about the 
+            //company and its manager and then prints it on the console.
+
+            string compName, compAddress, compPhoneNum, compFaxNum, compWebSite, compManager, managerFirstName, managerLastName, managerPhoneNum;
+
+            Console.Write("What is the companies name? ");
+            compName = Console.ReadLine();
+            Console.Write("What is the companies address? ");
+            compAddress = Console.ReadLine();
+            Console.Write("What is the companies phone #? ");
+            compPhoneNum = String.Format("{0:###-###-####}", int.Parse(Console.ReadLine()));
+            Console.Write("What is the companies fax #? ");
+            compFaxNum = String.Format("{0:###-###-####}", int.Parse(Console.ReadLine()));
+            Console.Write("What is the companies website? ");
+            compWebSite = Console.ReadLine();
+            Console.Write("What is the companies manager? ");
+            compManager = Console.ReadLine();
+
+            Console.Write("What is the manager's first name? ");
+            managerFirstName = Console.ReadLine();
+            Console.Write("What is the manager's last name? ");
+            managerLastName = Console.ReadLine();
+            Console.Write("What is the manager's phone #? ");
+            managerPhoneNum = String.Format("{0:###-###-####}", int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("{0} is located at {1} and can be reached at {2:###-###-####} or fax to {3:###-###-####}. Their website is {4} and is managed by {5}. {6} {7} can be reached at {8:###-###-####}", 
+                compName, compAddress, compPhoneNum, compFaxNum, compWebSite, compManager, managerFirstName, managerLastName, managerPhoneNum);
         }
     }
 }
