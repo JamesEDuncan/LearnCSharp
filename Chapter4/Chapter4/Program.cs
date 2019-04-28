564e6bcdb762230f398bed02cf35108ae6afe623
James Duncan <duncanje501@gmail.com>
2019-04-28 13:55:53 -0600

    sln p5

--- Chapter4/Chapter4/Program.cs
+++ Chapter4/Chapter4/Program.cs
@@ -10,10 +10,37 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-            //Write a program that reads from the console two integer numbers (int)
-            //and prints how many numbers between them exist, such that the
-            //remainder of their division by 5 is 0.Example: in the range(14, 25)
-            //there are 3 such numbers: 15, 20 and 25.
+            // 5. Write a program that reads from the console two integer numbers (int) and prints how many numbers between them exist, such that the
+            //    remainder of their division by 5 is 0.Example: in the range(14, 25) there are 3 such numbers: 15, 20 and 25.
+
+            Console.Write("Please enter an int: ");
+            int firstInt = int.Parse(Console.ReadLine());
+            Console.Write("Please enter another int: ");
+            int secondInt = int.Parse(Console.ReadLine());
+
+            int divByFiveCount = 0;
+
+            if (firstInt > secondInt)
+            {
+                do
+                {
+                    if ((firstInt % 5) == 0)
+                    {
+                        divByFiveCount++;
+                    }
+                } while (firstInt-- > secondInt);
+            }
+            else
+            {
+                do
+                {
+                    if ((secondInt % 5) == 0)
+                    {
+                        divByFiveCount++;
+                    }
+                } while (secondInt-- > firstInt);
+            }
+            Console.WriteLine("There are {0} number(s) perfectly divisible by 5 between those integers!", divByFiveCount);
         }
     }
 }
