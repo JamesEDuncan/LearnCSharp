1201dbd736dc9574f4011635d3522abadf30b716
James Duncan <duncanje501@gmail.com>
2019-06-01 16:56:50 -0600

    Just need error checking

--- Chapter4/Chapter4/Program.cs
+++ Chapter4/Chapter4/Program.cs
@@ -12,89 +12,17 @@ namespace Chapter4
         {
             // 7. Write a program that reads five integer numbers and prints their sum. If an invalid number is entered the program should prompt the user to enter another number.
 
-            long firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber;
-
-            //first num
-            Console.Write("Please enter a number of type int: ");
-            string parseString = Console.ReadLine();
+            int x = 0;
+            int tempInt = 0;
+            int sumInt = 0;
             do
             {
-                if (long.TryParse(parseString, out firstNumber) == false)
-                {
-                    Console.Write("Try a real int: ");
-                    parseString = Console.ReadLine();
-                }
-                else
-                {
-                    firstNumber = long.Parse(parseString);
-                }
-            } while (long.TryParse(parseString, out firstNumber) == false);
+                Console.WriteLine("Please enter a nubmer of type int");
+                tempInt = int.Parse(Console.ReadLine());
+                sumInt += tempInt;
+            } while (++x < 5);
 
-            //second num
-            Console.Write("Please enter a number of type int: ");
-            parseString = Console.ReadLine();
-            do
-            {
-                if (long.TryParse(parseString, out secondNumber) == false)
-                {
-                    Console.Write("Try a real int: ");
-                    parseString = Console.ReadLine();
-                }
-                else
-                {
-                    secondNumber = long.Parse(parseString);
-                }
-            } while (long.TryParse(parseString, out secondNumber) == false);
-
-            // third num
-            Console.Write("Please enter a number of type int: ");
-            parseString = Console.ReadLine();
-            do
-            {
-                if (long.TryParse(parseString, out thirdNumber) == false)
-                {
-                    Console.Write("Try a real int: ");
-                    parseString = Console.ReadLine();
-                }
-                else
-                {
-                    thirdNumber = long.Parse(parseString);
-                }
-            } while (long.TryParse(parseString, out thirdNumber) == false);
-
-            // fourth num
-            Console.Write("Please enter a number of type int: ");
-            parseString = Console.ReadLine();
-            do
-            {
-                if (long.TryParse(parseString, out fourthNumber) == false)
-                {
-                    Console.Write("Try a real int: ");
-                    parseString = Console.ReadLine();
-                }
-                else
-                {
-                    fourthNumber = long.Parse(parseString);
-                }
-            } while (long.TryParse(parseString, out fourthNumber) == false);
-
-            // fifth num
-            Console.Write("Please enter a number of type int: ");
-            parseString = Console.ReadLine();
-            do
-            {
-                if (long.TryParse(parseString, out fifthNumber) == false)
-                {
-                    Console.Write("Try a real int: ");
-                    parseString = Console.ReadLine();
-                }
-                else
-                {
-                    fifthNumber = long.Parse(parseString);
-                }
-            } while (long.TryParse(parseString, out fifthNumber) == false);
-
-            Console.WriteLine("The sum of those nubmers is {0}.", (firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber));
+            Console.WriteLine("The sum of the int's is " + sumInt);
         }
     }
 }
