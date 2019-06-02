971d2f4a68e4ebe6ca55af7ae20b901116a726d4
James Duncan <duncanje501@gmail.com>
2019-06-02 13:28:29 -0600

    sln p9

--- Chapter4/Chapter4/Program.cs
+++ Chapter4/Chapter4/Program.cs
@@ -10,8 +10,48 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-            //Write a program that reads an integer number n from the console. After
-            //that reads n numbers from the console and prints their sum.
+            //9. Write a program that reads an integer number "n" from the console. After that reads "n" numbers from the console and prints their sum.
+
+            int n, 
+                tempHoldIntToSum,
+                sumOfInts = 0, 
+                loopCounter = 0;
+            string prsStrErrChk;
+
+            Console.Write("How many numbers would you like to sum? ");
+            do
+            {
+                prsStrErrChk = Console.ReadLine();
+                if (int.TryParse(prsStrErrChk, out n))
+                {
+                    n = int.Parse(prsStrErrChk);
+                }
+                else
+                {
+                    Console.Write("Please format your answer as a numeric intiger: ");
+                }
+            } while (int.TryParse(prsStrErrChk, out n) == false);
+
+            do
+            {
+                Console.Write("Please enter one of the nubmers you want to sum: ");
+                do
+                {
+                    prsStrErrChk = Console.ReadLine();
+                    if (int.TryParse(prsStrErrChk, out tempHoldIntToSum))
+                    {
+                        tempHoldIntToSum = int.Parse(prsStrErrChk);
+                    }
+                    else
+                    {
+                        Console.Write("Please format your answer as a numeric intiger: ");
+                    }
+                } while (int.TryParse(prsStrErrChk, out tempHoldIntToSum) == false);
+
+                sumOfInts += tempHoldIntToSum;
+            } while (++loopCounter < n);
+
+            Console.WriteLine("The sum of the numbers is " + sumOfInts);
         }
     }
 }
