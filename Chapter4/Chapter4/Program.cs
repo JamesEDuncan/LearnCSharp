393d8a3a16d87e93560462ac029946eaca46cca9
James Duncan <duncanje501@gmail.com>
2019-06-02 13:04:26 -0600

    sln p8

--- Chapter4/Chapter4/Program.cs
+++ Chapter4/Chapter4/Program.cs
@@ -10,8 +10,33 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-            //Write a program that reads five numbers from the console and prints the
+            //8. Write a program that reads five numbers from the console and prints the
             //greatest of them.
+
+            int[] fiveInts = { 0, 1, 2, 3, 4 };
+            int largestInt;
+            int loopCounter;
+            
+            loopCounter = 0;
+            do
+            {
+                Console.Write("Please enter a number of type int: ");
+                fiveInts[loopCounter] = int.Parse(Console.ReadLine());
+            } while (++loopCounter <5);
+
+            loopCounter = 0;
+            int tempInt = fiveInts[loopCounter];
+            do
+            {
+                if (tempInt < fiveInts[++loopCounter])
+                {
+                    tempInt = fiveInts[loopCounter];
+                }
+            } while (loopCounter < 4);
+
+            largestInt = tempInt;
+            Console.WriteLine("The largest number is " + largestInt);
+
         }
     }
 }
