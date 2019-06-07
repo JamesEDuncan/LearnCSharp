800ff6bef622c5e07746ecd2f2fa1efa62cb86bf
James Duncan <duncanje501@gmail.com>
2019-06-07 11:06:16 -0600

    sln p12

--- Chapter4/Chapter4/Program.cs
+++ Chapter4/Chapter4/Program.cs
@@ -10,8 +10,30 @@ namespace Chapter4
     {
         static void Main(string[] args)
         {
-            //Write a program that calculates the sum (with precision of 0.001) of
-            //the following sequence: 1 + 1 / 2 - 1 / 3 + 1 / 4 - 1 / 5 + …
+            //12. Write a program that calculates the sum (with precision of 0.001) of the following sequence: 1 + 1/2 - 1/3 + 1/4 - 1/5 + …
+
+            double oldSum = 1,
+                   newSum,
+                   tempSum,
+                   n = 2,
+                   difference;
+
+            do
+            {
+                if (n % 2 == 0)
+                {
+                    newSum = oldSum + (1 / n++);
+                }
+                else
+                {
+                    newSum = oldSum - (1 / n++);
+                }
+
+                difference = Math.Abs(newSum - oldSum);
+                oldSum = newSum;
+            } while (difference > .001);
+
+            Console.WriteLine("The sum is {0: .###}", newSum);
         }
     }
 }
