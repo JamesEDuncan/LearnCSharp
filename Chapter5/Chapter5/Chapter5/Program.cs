91ffa7d7924a7738202c3848f081826519d720a6
James Duncan <duncanje501@gmail.com>
2019-06-10 16:21:52 -0600

    add P2

--- Chapter5/Chapter5/Chapter5/Program.cs
+++ Chapter5/Chapter5/Chapter5/Program.cs
@@ -10,6 +10,7 @@ namespace Chapter5
     {
         static void Main(string[] args)
         {
+            //2. Write a program that shows the sign (+ or -) of the product of three real numbers, without calculating it.Use a sequence of if operators.
         }
     }
 }
