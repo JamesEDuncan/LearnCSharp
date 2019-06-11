d2f1b93bc66c80736e46bda68ef0f0e341e098f2
James Duncan <duncanje501@gmail.com>
2019-06-11 16:48:29 -0600

    sln p2

--- Chapter5/Chapter5/Chapter5/Program.cs
+++ Chapter5/Chapter5/Chapter5/Program.cs
@@ -10,7 +10,53 @@ namespace Chapter5
     {
         static void Main(string[] args)
         {
-            //2. Write a program that shows the sign (+ or -) of the product of three real numbers, without calculating it.Use a sequence of if operators.
+            //2. Write a program that shows the sign (+ or -) of the product of three real numbers, without calculating it. Use a sequence of if operators.
+
+            double[] firstNumber = { 0, 1, 2};
+            int negNumCount = 0;
+            string tempNumParse;
+            
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Please enter a number: ");
+                do
+                {
+                    tempNumParse = Console.ReadLine();
+                    if (double.TryParse(tempNumParse, out double a))
+                    {
+                        firstNumber[i] = double.Parse(tempNumParse);
+                    }
+                    else
+                    {
+                        Console.Write("Please enter a valid number: ");
+                    }
+                } while (double.TryParse(tempNumParse, out double b) == false);
+            }
+
+            if (firstNumber[0] < 0)
+            {
+                negNumCount++;
+            }
+            if (firstNumber[1] < 0)
+            {
+                negNumCount++;
+            }
+            if (firstNumber[2] < 0)
+            {
+                negNumCount++;
+            }
+            if (firstNumber[0] == 0 || firstNumber[1] == 0 || firstNumber[2] == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else if (negNumCount == 0 || negNumCount == 2)
+            {
+                Console.WriteLine("+");
+            }
+            else
+            {
+                Console.WriteLine("-");
+            }
         }
     }
 }
